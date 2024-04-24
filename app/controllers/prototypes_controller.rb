c0b5d61bedf0cabcb4fac5eeae4322d456370738
y-uchi <bonbonheart@gmail.com>
2024-04-24 15:58:38 +0900

    投稿したらindexに戻るように修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -16,8 +16,7 @@ class PrototypesController < ApplicationController
   end
 
   def create
-    @prototype = Prototype.new
-    Prototype.create(prototype_params)
+    @prototype = Prototype.new(prototype_params)
     if @prototype.save
       redirect_to root_path
     else
