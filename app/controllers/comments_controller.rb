82a0a4828d16f16811ecbe3f45b905f1eb501bfe
y-uchi <bonbonheart@gmail.com>
2024-04-25 10:17:29 +0900

    コメントの修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,12 @@
 class CommentsController < ApplicationController
 
   def create
-    @prototype = Prototype.find(params[:prototype_id])
-    @comment = @prototype.comments.new(comment_params)
+    @comment = Comment.new(comment_params)
     if @comment.save
       redirect_to prototype_path(@prototype)
     else
+      @prototype = @comment.prototype
+      @comments = @prototype.comments
       render "prototypes/show", status: :unprocessable_entity
     end
   end
