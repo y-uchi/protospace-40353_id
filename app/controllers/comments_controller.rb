a848dd66318f87d6de2f5269a2d19be8d9a7bd11
y-uchi <bonbonheart@gmail.com>
2024-04-25 11:05:35 +0900

    コメント機能の修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      redirect_to prototype_path(@prototype)
+      redirect_to prototype_path(@comment.prototype)
     else
       @prototype = @comment.prototype
       @comments = @prototype.comments
